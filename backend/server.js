9fdc462a787b1473048127f7eda51dc4361f17b4
Zainab Tijani <jani.tijj@gmail.com>
2022-09-04 16:12:58 +0100

    initial commit and push of all files

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+const { Socket } = require('dgram');
+const express = require('express');
+const app = express();
+const {createServer, Server} = require('http');
+const server = createServer(app);
+
+const{server} = require("socket.io");
+const port = 3001;
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+    }
+});
+
+io.on('connection', socket => {
+    console.log(`a user connected: ${socket.id}`);
+    socket.on("json_room", ({user, room}) =>  {
+        socket.join(room)
+    })
+
+    socket.on("send-msg", ({room, user, message}) => {
+        const d = {user: user, message: message}
+        socket.to(room).emit("receive_msg", d);
+    });
+});
+
+server.listen(port, () => {
+    console.log(`listening on ${port}`);
+});
---
+++ frontend/src/App.js
@@ -0,0 +1,93 @@
+import { useState, useEffect } from 'react';
+import io from "socket.io-client";
+import { v4 } from 'uuid';
+import './App.css';
+
+
+const PORT = 3001;
+const socket = io(`http://localhost:${PORT}`);
+
+function App() {
+
+  const [isConnected, setIsConnected] = useState(socket.isConnected);
+  const [user, setUser] = useState("");
+  const [room, setRoom] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState("");
+  const [chatIsVisible, setChatIsVisible] = useState(false);
+
+
+  useEffect(() => {
+    
+    socket.on('connect', () => {
+      setIsConnected(true);
+    });
+    socket.on('disconnect', () => {
+      setIsConnected(false);
+    });
+    // socket.on('receive_msg', (msg) => {
+    //   setNewMessage((messages) => [...messages, msg]);
+    // });
+  
+    return () => {
+      socket.off('connect');
+      socket.off('disconnect');
+    }
+  
+  }, [isConnected]) //newMessage]),
+
+  
+  useEffect(() => {
+    socket.on("receive_msg", ({user, message}) => {
+      const msg = `${user} send: ${message}`;
+      setMessages(prevState => [msg, ...prevState]);
+    });
+  }, [socket, messages])
+
+
+  const handleEnterChatRoom = () => {
+    if (user !== "" && room !== "") {
+      socket.emit("join_room", {user, room});
+      setChatIsVisible(true);
+    }
+  }
+
+  const handleSendMessage = () => {
+    const newMsgData = {
+      room: room,
+      user: user,
+      message: newMessage
+    }
+    socket.emit("send-msg", newMsgData);
+    const msg = `${user} send: ${newMessage}`;
+    setMessages(prevState => [msg, ...prevState]);
+    setNewMessage("");
+
+  }
+    
+
+  return (
+    <div className="main">  
+        <h1 className="logo">Chat (...in groups 😉)</h1>
+        { !chatIsVisible ? 
+          <>
+            <input className="input-field" type="text" placeholder="Enter your name" value={user} onChange={(e) => setUser(e.target.value)} />
+            <input className="input-field" type="text" placeholder="Enter room name" value={room} onChange={(e) => setRoom(e.target.value)} />
+            <button className="btn-chat" onClick={handleEnterChatRoom}>Enter chat room</button>
+          </>
+          :
+          <>
+            <h5>Room: {room} | User: {user}</h5>
+            <div className="chat-box">
+              {messages.map((el => <div key={v4()}>{el}</div>))}
+            </div>
+
+            <input type="text" placeholder="Enter message" value={newMessage} onChange={(e) => setNewMessage(e.target.value)} />
+            <button onClick={handleSendMessage}>Send message</button>
+          </>
+        }
+    </div>
+  );
+}
+
+export default App;
